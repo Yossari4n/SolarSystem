56f4b158a8dc0f9e1273ec8a676253047901768c
Nerovski <j.stokowski97@gmail.com>
2019-01-31 16:14:07 +0100

    ThirdPersonController component

---
+++ src/cbs/components/ThirdPersonController.cpp
@@ -0,0 +1,61 @@
+#include "ThirdPersonController.h"
+
+ThirdPersonController::ThirdPersonController(class Object* target, float radius, glm::vec3 front, float mouse_sensitivity)
+    : m_Target(target)
+    , m_Radius(radius)
+    , m_Front(front)
+    , m_MouseSensitivity(mouse_sensitivity) {
+    m_XRotation = 0.0f;
+    m_YRotation = 0.0f;
+    m_RotationAxis = glm::vec3(-1.0f, 0.0f, 0.0f);
+}
+
+void ThirdPersonController::Update() {
+    // Get mouse input and accumulate it as rotations
+    m_XRotation += glm::radians(g_Input.MouseOffset().y * m_MouseSensitivity);
+    m_YRotation += glm::radians(g_Input.MouseOffset().x * m_MouseSensitivity);
+    
+    // Get whole rotation around Y
+    glm::quat rot_y(glm::vec3(0.0f, m_YRotation, 0.0f));
+    
+    // Create new arbitrary X axis
+    glm::vec3 curr_rotation_axis = m_RotationAxis * rot_y;
+    
+    // Rotate camera around world origin and then move it to m_Target
+    glm::vec3 new_pos = glm::vec3(0.0f, 0.0f, m_Radius) * rot_y;
+    new_pos = glm::rotate(new_pos, m_XRotation, curr_rotation_axis);
+    new_pos = new_pos + m_Target->Transform().Position();
+    
+    // Calculate rotation from old front to new front
+    glm::vec3 diff = m_Target->Transform().Position() - new_pos;
+    glm::quat front_rot = m_RotationBeetwen(m_Front, diff);
+    
+    // Apply
+    Object().Transform().Position(new_pos);
+    Object().Transform().Rotation(front_rot);
+}
+
+glm::quat ThirdPersonController::m_RotationBeetwen(const glm::vec3 &start, const glm::vec3 &dest) {
+    glm::vec3 n_start = glm::normalize(start);
+    glm::vec3 n_dest = glm::normalize(dest);
+    
+    float cos_theta = glm::dot(n_start, n_dest);
+    glm::vec3 rotation_axis;
+    
+    if (cos_theta < -1 + 0.001f) {
+        rotation_axis = cross(glm::vec3(0.0f, 0.0f, 1.0f), n_start);
+        if (length2(rotation_axis) < 0.01) {
+            rotation_axis = cross(glm::vec3(1.0f, 0.0f, 0.0f), n_start);
+        }
+        
+        rotation_axis = normalize(rotation_axis);
+        return glm::angleAxis(glm::radians(180.0f), rotation_axis);
+    }
+    
+    rotation_axis = cross(n_start, n_dest);
+    
+    float s = sqrt((1 + cos_theta) * 2);
+    float invs = 1 / s;
+    
+    return glm::quat(s * 0.5f, rotation_axis.x * invs, rotation_axis.y * invs, rotation_axis.z * invs);
+}
---
+++ src/cbs/components/ThirdPersonController.h
@@ -0,0 +1,35 @@
+#ifndef ThirdPersonController_h
+#define ThirdPersonController_h
+
+#include "IComponent.h"
+
+#include "../../utilities/Time.h"
+#include "../../utilities/Input.h"
+
+#include <glm/gtx/rotate_vector.hpp>
+
+class ThirdPersonController : public IComponent {
+public:
+    ThirdPersonController(class Object* target, float radius, glm::vec3 front = glm::vec3(0.0f, 0.0f, -1.0f), float mouse_sensitivity = 0.1f);
+    
+    void Update() override;
+    
+    void Target(class Object* target) { m_Target = target; }
+    void Radius(float radius) { m_Radius = radius; }
+    
+    class Object* line;
+    
+private:
+    class Object* m_Target;
+    float m_Radius;
+    float m_MouseSensitivity;
+    
+    glm::vec3 m_Front;
+    glm::vec3 m_RotationAxis;
+    float m_XRotation;
+    float m_YRotation;
+    
+    glm::quat m_RotationBeetwen(const glm::vec3& start, const glm::vec3& dest);
+};
+
+#endif
