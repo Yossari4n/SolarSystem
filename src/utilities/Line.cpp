e61cc3c033829a8abf43bcc957d5be6b7762bff4
Jakub Stokowski <j.stokowski97@gmail.com>
2019-02-02 19:53:58 +0100

    Line class

--- src/utilities/Line.cpp
+++ src/utilities/Line.cpp
@@ -1,1 +1,37 @@
 #include "Line.h"
+
+Line::Line(glm::vec3 start, glm::vec3 end, glm::vec3 color, float width)
+    : m_Start(start)
+    , m_End(end)
+    , m_Color(color)
+    , m_Width(width) {
+    m_ShaderType = ShaderProgram::TYPE::COLOR_PURE;
+    
+    float vertices[] = { start.x, start.y, start.z, end.x, end.y, end.z };
+    
+    glGenVertexArrays(1, &m_VAO);
+    glGenBuffers(1, &m_VBO);
+    
+    glBindVertexArray(m_VAO);
+    
+    glBindBuffer(GL_ARRAY_BUFFER, m_VBO);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+    
+    // Position
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void *)0);
+    glEnableVertexAttribArray(0);
+    
+    glBindVertexArray(0);
+    glBindBuffer(GL_ARRAY_BUFFER, 0);
+}
+
+void Line::Draw(const ShaderProgram &shader) const {
+    glm::mat4 model(1.0f);
+    shader.SetMat4("model", model);
+    shader.SetVec3("color", m_Color);
+    
+    glBindVertexArray(m_VAO);
+    glLineWidth(m_Width);
+    glDrawArrays(GL_LINES, 0, 6);
+    glBindVertexArray(0);
+}
--- src/utilities/Line.h
+++ src/utilities/Line.h
@@ -4,7 +4,19 @@
 #include "../rendering/IDrawable.h"
 
 class Line : public IDrawable {
+public:
+    Line(glm::vec3 start, glm::vec3 end, glm::vec3 color, float width = 1.0f);
     
+    void Draw(const ShaderProgram& shader) const override;
+    
+private:
+    glm::vec3 m_Start;
+    glm::vec3 m_End;
+    glm::vec3 m_Color;
+    float m_Width;
+    
+    unsigned int m_VAO;
+    unsigned int m_VBO;
 };
 
 #endif
