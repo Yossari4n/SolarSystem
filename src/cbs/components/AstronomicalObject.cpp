d7b3a9f091e45993301a5046b8dd5d1937b1c561
Nerovski <j.stokowski97@gmail.com>
2019-07-15 13:48:52 +0200

    Basic lerp between positions

--- src/cbs/components/AstronomicalObject.cpp
+++ src/cbs/components/AstronomicalObject.cpp
@@ -2,24 +2,46 @@
 
 #include "MeshRenderer/MeshRenderer.h"
 
-AstronomicalObject::AstronomicalObject(float N1, float N2, float i1, float i2, float w1, float w2, float a1, float a2, float e1, float e2, float M1, float M2)
+AstronomicalObject::AstronomicalObject(OrbitalElements orbital_elements)
 	: m_RawTime(time(0))
-	, m_N1(N1)
-	, m_N2(N2)
-	, m_i1(N2)
-	, m_i2(i2)
-	, m_w1(w1)
-	, m_w2(w2)
-	, m_a1(a1)
-	, m_a2(a2)
-	, m_e1(e1)
-	, m_e2(e2)
-	, m_M1(M1)
-	, m_M2(M2) {
+	, m_StartingPos(0.0f)
+	, m_EndPos(0.0f)
+	, m_LerpTime(0.0f)
+	, m_N1(orbital_elements.N1)
+	, m_N2(orbital_elements.N2)
+	, m_i1(orbital_elements.N2)
+	, m_i2(orbital_elements.i2)
+	, m_w1(orbital_elements.w1)
+	, m_w2(orbital_elements.w2)
+	, m_a1(orbital_elements.a1)
+	, m_a2(orbital_elements.a2)
+	, m_e1(orbital_elements.e1)
+	, m_e2(orbital_elements.e2)
+	, m_M1(orbital_elements.M1)
+	, m_M2(orbital_elements.M2) {
+}
+
+void AstronomicalObject::Initialize() {
+	m_StartingPos = m_EndPos = Position();
+}
+
+void AstronomicalObject::Update() {
+	m_LerpTime = m_LerpTime + g_Time.DeltaTime();
+	
+	Object().Transform().Position(glm::lerp<float>(m_StartingPos, m_EndPos, m_LerpTime));
+
+	if (m_LerpTime > 1.0f) {
+		m_RawTime = m_RawTime + DAY_TO_SEC * static_cast<std::time_t>(g_Time.TimeMultiplayer());
+
+		m_StartingPos = m_EndPos;
+		m_EndPos = Position();
+
+		m_LerpTime = 0.0f;
+	}
 }
 
 // Implementation based on https://stjarnhimlen.se/comp/ppcomp.html
-void AstronomicalObject::Update() {
+glm::vec3 AstronomicalObject::Position() const {
 	const int d = TimeScale();
 
 	float N = Rev(m_N1 + m_N2 * d);		// Longitude of the ascending node
@@ -28,7 +50,7 @@ void AstronomicalObject::Update() {
 	float a = m_a1 + m_a2 * d;			// Mean distance from Sun
 	float e = m_e1 + m_e2 * d;			// Eccentricity (0=circle, 0-1=ellipse, 1=parabola)
 	float M = Rev(m_M1 + m_M2 * d);		// Mean anomaly (0 at perihelion; increases uniformly with time)
-	
+
 	float E = M + e * (180.0f / M_PIf) * sin(glm::radians(M)) * (1.0f + e * cos(glm::radians(M)));	// Eccentric anomaly
 
 	// Rectangular coordinates
@@ -37,7 +59,7 @@ void AstronomicalObject::Update() {
 
 	float r = sqrt(pow(xv, 2) + pow(yv, 2));	// Distance
 	float v = glm::degrees(atan2(xv, yv));		// True anomaly
-	
+
 	// Convert to radians
 	float N_rad = glm::radians(N);
 	float i_rad = glm::radians(i);
@@ -50,17 +72,17 @@ void AstronomicalObject::Update() {
 	h_pos.z = r * (sin(N_rad) * cos(vw_rad) + cos(N_rad) * sin(vw_rad) * cos(i_rad));
 	h_pos = h_pos * ASTRONOMICAL_UNIT;
 
-	Object().Transform().Position(h_pos);
+	return h_pos;
 }
 
-int AstronomicalObject::TimeScale() {
+int AstronomicalObject::TimeScale() const {
 	std::tm date;
 	localtime_s(&date, &m_RawTime);
 
 	const int Y = date.tm_year + 1900;
 	const int M = date.tm_mon + 1;
 	const int D = date.tm_mday;
-	
+
 	return 367 * Y - (7 * (Y + ((M + 9) / 12))) / 4 + (275 * M) / 9 + D - 730530;
 }
 
--- src/cbs/components/AstronomicalObject.h
+++ src/cbs/components/AstronomicalObject.h
@@ -9,8 +9,7 @@
 
 #pragma warning(push, 0)
 #include <glm/glm.hpp>
-#include <glm/glm.hpp>
-#include <glm/gtc/matrix_transform.hpp>
+#include <glm/gtx/compatibility.hpp>
 #pragma warning(pop)
 
 #include <iostream>
@@ -20,20 +19,67 @@
 
 constexpr float M_PIf = static_cast<float>(M_PI);
 constexpr float ASTRONOMICAL_UNIT = 150.0f / 3.0f;
+constexpr time_t DAY_TO_SEC = 60 * 60 * 24;
+
+struct OrbitalElements {
+	OrbitalElements(float N1, float N2, float i1, float i2, float w1, float w2, float a1, float a2, float e1, float e2, float M1, float M2)
+		: N1(N1)
+		, N2(N2)
+		, i1(i1)
+		, i2(i2)
+		, w1(w1)
+		, w2(w2)
+		, a1(a1)
+		, a2(a2)
+		, e1(e1)
+		, e2(e2)
+		, M1(M1)
+		, M2(M2) {
+	}
+
+	// Longitude of the ascending node
+	float N1;
+	float N2;
+	// Inclination to the ecliptic (plane of the Earth's orbit)
+	float i1;
+	float i2;
+	// Argument of perihelion
+	float w1;
+	float w2;
+	// Semi-major axis, or mean distance from Sun
+	float a1;
+	float a2;
+	// Eccentricity(0 = circle, 0 - 1 = ellipse, 1 = parabola)
+	float e1;
+	float e2;
+	// Mean anomaly
+	float M1;
+	float M2;
+};
 
 class AstronomicalObject : public IComponent {
 public:
-	AstronomicalObject(float N1, float N2, float i1, float i2, float w1, float w2, float a1, float a2, float e1, float e2, float M1, float M2);
+	AstronomicalObject(OrbitalElements orbital_elements);
     
+	void Initialize() override;
     void Update() override;
 
 private:
-	int TimeScale();
-	float Rev(float x) {
+	glm::vec3 Position() const;
+	int TimeScale() const;
+
+	float Rev(float x) const {
 		return x - floor(x / 360.0f) * 360.0f;
 	}
 
 	std::time_t m_RawTime;
+
+	// Positions for lerp
+	float m_LerpTime;
+	glm::vec3 m_StartingPos;
+	glm::vec3 m_EndPos;
+
+	// Orbital elements
 	float m_N1;
 	float m_N2;
 	float m_i1;
